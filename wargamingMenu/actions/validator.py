539bb93f481c76c6bdc4a7d938e9744548c80d2e
yurus2015 <ironimplant@gmail.ckm>
2022-10-26 11:55:04 +0300

    rename ta_validator to validator

--- wargamingMenu/actions/validator.py
+++ wargamingMenu/actions/validator.py
@@ -2,11 +2,11 @@ action_category = "Main Tools"
 action_label = "Validator"
 action_icon = "icon_validator.png"
 
-packages = ['ta_validator']
+packages = ['validator']
 for i in sys.modules.keys():
     for package in packages:
         if i.startswith(package):
             del sys.modules[i]
 
-import ta_validator.main as vld
-vld.main(autoload = False)
+import validator.main as vld
+vld.main(autoload=False)
